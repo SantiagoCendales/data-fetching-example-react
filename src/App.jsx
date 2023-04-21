7b9ac6b6efba35b2330ab970630556ea75a1447f
Santiago Cendales <srcendales94@gmail.com>
2023-04-21 17:32:24 -0500

    Refactor use effect

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,42 @@ export const App = () => {
   useEffect(() => {
     fetch(CAT_RANDOM_FACT)
       .then((data) => {
+        console.log(data)
         data.json()
           .then((resp) => {
             const { fact } = resp
             setFact(fact)
-            const firstWord = fact.split(' ', 3).join(' ')
-            fetch(`${BASE_CAT_API_URL}cat/says/${firstWord}?size=50&color=red&json=true`)
-              .then(catImgResp => catImgResp.json()
-                .then(img => {
-                  console.log(img)
-                  const { url } = img
-                  setImageUrl(url)
-                })
-              )
           })
       })
   }, [])
 
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ', 3).join(' ')
+    fetch(`${BASE_CAT_API_URL}cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then(catImgResp => catImgResp.json()
+        .then(img => {
+          console.log(img)
+          const { url } = img
+          setImageUrl(url)
+        })
+      )
+  }, [fact])
+
   return (
     <>
-      <h1>App de fetching de data</h1>
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${BASE_CAT_API_URL}${imageUrl}`} alt={`Cat image with three first words of ${fact}`} />}
+      <main style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        height: '100vh'
+      }}
+      >
+        <h1>App de fetching de data</h1>
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img style={{ width: '350px' }} src={`${BASE_CAT_API_URL}${imageUrl}`} alt={`Cat image with three first words of ${fact}`} />}
+      </main>
     </>
   )
 }
