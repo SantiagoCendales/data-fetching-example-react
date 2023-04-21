a2d77ca586ce05cd19bdefae23890c66288f682b
Santiago Cendales <srcendales94@gmail.com>
2023-04-21 15:17:15 -0500

    Add fetch cat image and it is displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from 'react'
 
 const CAT_RANDOM_FACT = 'https://catfact.ninja/fact'
-// const API_URL = `https://cataas.com/cat/says/${text}?size=50&color=red`
+const BASE_CAT_API_URL = 'https://cataas.com/'
 
 export const App = () => {
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_RANDOM_FACT)
       .then((data) => {
         data.json()
           .then((resp) => {
-            setFact(resp.fact)
+            const { fact } = resp
+            setFact(fact)
+            const firstWord = fact.split(' ', 3).join(' ')
+            fetch(`${BASE_CAT_API_URL}cat/says/${firstWord}?size=50&color=red&json=true`)
+              .then(catImgResp => catImgResp.json()
+                .then(img => {
+                  console.log(img)
+                  const { url } = img
+                  setImageUrl(url)
+                })
+              )
           })
       })
   }, [])
@@ -20,6 +31,7 @@ export const App = () => {
     <>
       <h1>App de fetching de data</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${BASE_CAT_API_URL}${imageUrl}`} alt={`Cat image with three first words of ${fact}`} />}
     </>
   )
 }
