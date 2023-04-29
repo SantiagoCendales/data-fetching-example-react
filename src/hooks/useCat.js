1869e13d1ed6eb11100a14acc20ceb6559b0408d
Santiago Cendales <srcendales94@gmail.com>
2023-04-29 16:38:20 -0500

    Refactor useCat hook

--- src/hooks/useCat.js
+++ src/hooks/useCat.js
@@ -4,19 +4,15 @@ import { getCatFact } from '../services/facts'
 export const useCat = () => {
   const [fact, setFact] = useState(null)
 
-  useEffect(() => {
-    // const getFact = async () => {
-    //   const catFactResp = await getCatFact()
-    //   setFact(catFactResp)
-    // }
-    // getFact() <====== Código con async await
-    getCatFact().then((data) => setFact(data))
-  }, [])
-
   const getNewFact = async () => {
     const newFact = await getCatFact()
     setFact(newFact)
   }
+
+  useEffect(() => {
+    getNewFact()
+  }, [])
+
   return {
     fact,
     getNewFact
